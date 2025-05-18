2ea847f9148f79d7f9aa7a2d0199a78dd86ac497
IroshanaRavishan <bandarairoshana@gmail.com>
2025-05-18 21:27:46 +0530

    added interface class of the Book Service

---
+++ FilmVault/Services/IFilmService.cs
@@ -0,0 +1,13 @@
+﻿using BookVault.DTOs;
+
+namespace BookVault.Services
+{
+    public interface IBookService
+    {
+        Task<BookDto> CreateMovieAsync(CreateBookDto command);
+        Task<BookDto?> GetMovieByIdAsync(Guid id);
+        Task<IEnumerable<BookDto>> GetAllMoviesAsync();
+        Task UpdateMovieAsync(Guid id, UpdateBookDto command);
+        Task DeleteMovieAsync(Guid id);
+    }
+}
