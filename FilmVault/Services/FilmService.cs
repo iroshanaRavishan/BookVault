31479fdaec8a80f3abd210afc97171c146725946
IroshanaRavishan <bandarairoshana@gmail.com>
2025-05-18 21:29:48 +0530

    implemented basic implementation of IService and added actual implementation of CreateMovieAsync method

---
+++ FilmVault/Services/FilmService.cs
@@ -0,0 +1,54 @@
+﻿using FilmVault.Data;
+using FilmVault.DTOs;
+using FilmVault.Models;
+
+namespace FilmVault.Services
+{
+    public class FilmService : IFilmService
+    {
+        private readonly FilmDbContext _dbContext;
+        private readonly ILogger<FilmService> _logger;
+
+        public FilmService(FilmDbContext dbContext, ILogger<FilmService> logger)
+        {
+            _dbContext = dbContext;
+            _logger = logger;
+        }
+
+        public async Task<FilmDto> CreateMovieAsync(CreateFilmDto command)
+        {
+            var film = Film.Create(command.Name, command.Genre, command.ReleaseDate, command.Rating);
+
+            await _dbContext.Films.AddAsync(film);
+            await _dbContext.SaveChangesAsync();
+
+            return new FilmDto(
+               film.Id,
+               film.Name,
+               film.Genre,
+               film.ReleaseDate,
+               film.Rating
+            );
+        }
+
+        public Task DeleteMovieAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<FilmDto>> GetAllMoviesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FilmDto?> GetMovieByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateMovieAsync(Guid id, UpdateFilmDto command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
