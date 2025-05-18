6d3b633c3a83d47651e70549413f5dbf4d3dfce9
IroshanaRavishan <bandarairoshana@gmail.com>
2025-05-18 21:27:46 +0530

    added interface class of the Film Service

---
+++ FilmVault/Services/IFilmService.cs
@@ -0,0 +1,13 @@
+﻿using FilmVault.DTOs;
+
+namespace FilmVault.Services
+{
+    public interface IFilmService
+    {
+        Task<FilmDto> CreateMovieAsync(CreateFilmDto command);
+        Task<FilmDto?> GetMovieByIdAsync(Guid id);
+        Task<IEnumerable<FilmDto>> GetAllMoviesAsync();
+        Task UpdateMovieAsync(Guid id, UpdateFilmDto command);
+        Task DeleteMovieAsync(Guid id);
+    }
+}
