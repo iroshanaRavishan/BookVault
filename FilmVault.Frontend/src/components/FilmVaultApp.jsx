dc11e63040f770815ea521c7969eb1c9e4f8c0c2
IroshanaRavishan <bandarairoshana@gmail.com>
2025-05-19 21:31:38 +0530

    added a page to delete images

--- FilmVault.Frontend/src/components/FilmVaultApp.jsx
+++ FilmVault.Frontend/src/components/FilmVaultApp.jsx
@@ -10,6 +10,7 @@ import LandingPage from './LandingPage'
 import CreateFilm from './CreateFilm';
 import EditFilms from './EditFilms';
 import GetFilm from './GetFilm';
+import DeleteFilms from './DeleteFilms';
 import NavBar from './NavBar';
 
 export default function FilmVaultApp() {
@@ -23,6 +24,7 @@ export default function FilmVaultApp() {
             <Route path="/create" element={<CreateFilm />} />
             <Route path="/edit" element={<EditFilms />} />
             <Route path="/search" element={<GetFilm />} />
+            <Route path="/delete" element={<DeleteFilms />} />
           </Routes>
         </div>
       </div>
