498c253b2734df838c7a7e3b5c1febd08deeca1d
IroshanaRavishan <bandarairoshana@gmail.com>
2025-05-19 21:30:19 +0530

    added a page to get the images by ID

--- FilmVault.Frontend/src/components/FilmVaultApp.jsx
+++ FilmVault.Frontend/src/components/FilmVaultApp.jsx
@@ -9,6 +9,7 @@ import styles from './componentstyles.module.css';
 import LandingPage from './LandingPage'
 import CreateFilm from './CreateFilm';
 import EditFilms from './EditFilms';
+import GetFilm from './GetFilm';
 import NavBar from './NavBar';
 
 export default function FilmVaultApp() {
@@ -21,6 +22,7 @@ export default function FilmVaultApp() {
             <Route path="/" element={<LandingPage />} />
             <Route path="/create" element={<CreateFilm />} />
             <Route path="/edit" element={<EditFilms />} />
+            <Route path="/search" element={<GetFilm />} />
           </Routes>
         </div>
       </div>
