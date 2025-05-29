d7c926fcbe3c413e6aa6617624b06f637696c210
IroshanaRavishan <bandarairoshana@gmail.com>
2025-05-29 15:56:07 +0530

    added interface of the default user profile picture repository

---
+++ BookVault.Backend/BookVault.Domain/Interfaces/IDefaultUserProfilePictureRepository.cs
@@ -0,0 +1,21 @@
+﻿using BookVault.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookVault.Domain.Interfaces
+{
+    public interface IDefaultUserProfilePictureRepository
+    {
+        Task<int> UploadImageAsync(IFormFile file);
+        Task<DefaultUserProfileImage?> GetImageByIdAsync(int id);
+        Task<IEnumerable<DefaultUserProfileImage>> GetAllImagesAsync();
+        Task<IEnumerable<DefaultUserProfileImage>> GetAllImagesWithDataAsync();
+        Task<IEnumerable<byte[]>> GetAllImageDataAsync();
+        Task<byte[]?> GetImageDataByIdAsync(int id);
+        Task<bool> DeleteImageAsync(int id);
+    }
+}
