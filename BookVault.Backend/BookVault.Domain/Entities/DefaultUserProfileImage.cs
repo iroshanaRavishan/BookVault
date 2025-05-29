1ecc38c3d182f2cf71f8c1fdda24cbd8cd506351
IroshanaRavishan <bandarairoshana@gmail.com>
2025-05-29 14:49:39 +0530

    added a modal for DefaultUserProfilePicture

---
+++ BookVault.Backend/BookVault.Domain/Entities/DefaultUserProfileImage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookVault.Domain.Entities
+{
+    public class DefaultUserProfileImage
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FileName { get; set; }
+
+        [Required]
+        public string ContentType { get; set; }
+
+        [Required]
+        public byte[] Data { get; set; }
+    }
+}
