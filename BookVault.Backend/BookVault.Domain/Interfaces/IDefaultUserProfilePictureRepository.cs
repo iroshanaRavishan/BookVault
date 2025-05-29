8f5050e84b9b3f063d18418f719bc39d3a07d62d
IroshanaRavishan <bandarairoshana@gmail.com>
2025-05-29 18:16:25 +0530

    renamed from 'DefaultUserProfileImage' to 'DefaultUserProfilePicture'

--- BookVault.Backend/BookVault.Domain/Interfaces/IDefaultUserProfilePictureRepository.cs
+++ BookVault.Backend/BookVault.Domain/Interfaces/IDefaultUserProfilePictureRepository.cs
@@ -11,9 +11,9 @@ namespace BookVault.Domain.Interfaces
     public interface IDefaultUserProfilePictureRepository
     {
         Task<int> UploadImageAsync(IFormFile file);
-        Task<DefaultUserProfileImage?> GetImageByIdAsync(int id);
-        Task<IEnumerable<DefaultUserProfileImage>> GetAllImagesAsync();
-        Task<IEnumerable<DefaultUserProfileImage>> GetAllImagesWithDataAsync();
+        Task<DefaultUserProfilePicture?> GetImageByIdAsync(int id);
+        Task<IEnumerable<DefaultUserProfilePicture>> GetAllImagesAsync();
+        Task<IEnumerable<DefaultUserProfilePicture>> GetAllImagesWithDataAsync();
         Task<IEnumerable<byte[]>> GetAllImageDataAsync();
         Task<byte[]?> GetImageDataByIdAsync(int id);
         Task<bool> DeleteImageAsync(int id);
