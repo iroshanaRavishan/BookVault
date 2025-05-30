6db3fe0f19a1b1d46ba746f7858cf415a61f3ebf
IroshanaRavishan <bandarairoshana@gmail.com>
2025-05-30 18:30:33 +0530

    added the new controller for Default User Profile Picture

---
+++ BookVault.Backend/BookVault/Controllers/DefaultUserProfilePictureController.cs
@@ -0,0 +1,17 @@
+﻿using BookVault.Domain.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookVault.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultUserProfilePictureController : ControllerBase
+    {
+        private readonly IDefaultUserProfilePictureRepository _defaultUserProfilePictureService;
+        public DefaultUserProfilePictureController(IDefaultUserProfilePictureRepository defaultUserProfilePictureService)
+        {
+            _defaultUserProfilePictureService = defaultUserProfilePictureService;
+        }
+    }
+}
