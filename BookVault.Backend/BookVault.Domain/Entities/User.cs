bfd8f204ebb8f05548f89ad5c33d166c38ed6cff
IroshanaRavishan <bandarairoshana@gmail.com>
2025-06-01 00:10:57 +0530

    added the User Model and the reqiored

---
+++ BookVault.Backend/BookVault.Domain/Entities/User.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookVault.Domain.Entities
+{
+    public class User : IdentityUser
+    {
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+
+        public DateTime ModifiedDate { get; set; } = DateTime.UtcNow;
+
+        public DateTime LastLogin { get; set; } = DateTime.UtcNow;
+
+        public bool IsAdmin { get; set; } = false;
+
+        public byte[] ProfilePicture { get; set; }
+
+        public string ProfilePictureContentType { get; set; }
+    }
+}
