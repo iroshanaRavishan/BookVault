c3e5b78b5205c3594c9068c6e93db809d6d7635a
IroshanaRavishan <bandarairoshana@gmail.com>
2025-06-01 00:17:05 +0530

    added the DTOs of Auth (User)

---
+++ BookVault.Backend/BookVault.Application/DTOs/AuthDTOs/UserRegistrationDto.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookVault.Application.DTOs.AuthDTOs
+{
+    public class UserRegistrationDto
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public IFormFile ProfilePicture { get; set; }
+    }
+}
