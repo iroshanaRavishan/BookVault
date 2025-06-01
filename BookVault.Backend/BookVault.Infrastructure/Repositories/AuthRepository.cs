90de642ee2d804fcd65bd9ee4ba69bf0d6e35eb0
IroshanaRavishan <bandarairoshana@gmail.com>
2025-06-01 23:51:44 +0530

    added the implementation of the auth interface class of the repository

---
+++ BookVault.Backend/BookVault.Infrastructure/Repositories/AuthRepository.cs
@@ -0,0 +1,69 @@
+﻿using BookVault.Domain.Entities;
+using BookVault.Domain.Interfaces;
+using BookVault.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookVault.Infrastructure.Repositories
+{
+
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly ILogger<AuthRepository> _logger;
+        public AuthRepository(UserManager<User> userManager, SignInManager<User> signInManager, ILogger<AuthRepository> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public async Task<User> GetAuthenticatedUserAsync(ClaimsPrincipal user)
+        {
+            try
+            {
+                return await _signInManager.UserManager.GetUserAsync(user);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error retrieving the user: {Message}", ex.Message);
+                throw;
+            }
+        }
+
+        public Task<User> GetProfilePictureAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<(bool IsSuccess, string Message)> LoginUserAsync(User userLogin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task LogoutUserAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IdentityResult> RegisterUserAsync(User user, string password)
+        {
+            try
+            {
+                return await _userManager.CreateAsync(user, password);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error retrieving the user: {Message}", ex.Message);
+                throw;
+            }
+        }
+    }
+}
