b6795f6a1f0be1b7a8d97cc904be57e37d5f438d
IroshanaRavishan <bandarairoshana@gmail.com>
2025-06-01 23:55:32 +0530

    added an action method to register a user

--- BookVault.Backend/BookVault/Controllers/AuthController.cs
+++ BookVault.Backend/BookVault/Controllers/AuthController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookVault.Application.DTOs.AuthDTOs;
+using BookVault.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookVault.API.Controllers
 {
@@ -6,5 +8,33 @@ namespace BookVault.API.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        [Consumes("multipart/form-data")]
+        public async Task<IActionResult> RegisterUser([FromForm] UserRegistrationDto user)
+        {
+            try
+            {
+                var result = await _authService.RegisterUserAsync(user);
+                if (!result.Succeeded)
+                {
+                    return BadRequest(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new
+                {
+                    message = $"Something went wrong, Please try again later. {ex.Message}",
+                    isSuccess = false
+                });
+            }
+            return Ok(new { message = "Registration Successful!", isSuccess = true });
+        }
     }
 }
