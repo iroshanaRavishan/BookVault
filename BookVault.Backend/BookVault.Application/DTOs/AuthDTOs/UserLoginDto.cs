4a70535db42af9be3c4d6af1971d01925aadd079
IroshanaRavishan <bandarairoshana@gmail.com>
2025-06-03 00:47:31 +0530

    removed the user name from the login DTO. (to login in via email and password)

--- BookVault.Backend/BookVault.Application/DTOs/AuthDTOs/UserLoginDto.cs
+++ BookVault.Backend/BookVault.Application/DTOs/AuthDTOs/UserLoginDto.cs
@@ -8,7 +8,6 @@ namespace BookVault.Application.DTOs.AuthDTOs
 {
     public class UserLoginDto
     {
-        public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
     }
