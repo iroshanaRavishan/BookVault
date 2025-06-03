9362aeb6a1eb636f9d94da507634b6a61a7fc7cf
IroshanaRavishan <bandarairoshana@gmail.com>
2025-06-04 01:15:39 +0530

    used the ProtectedRouter and Layout to secure and give the bettter layout to the applicaton

--- BookVault.Frontend/src/components/root-book-vault/BookVaultApp.jsx
+++ BookVault.Frontend/src/components/root-book-vault/BookVaultApp.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Routes,
-  Route
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider
 } from 'react-router-dom';
-import styles from './bookvaultapp.module.css';
 
 import LandingPage from '../landing-page/LandingPage'
 import CreateBook from '../create-books/CreateBook';
@@ -13,24 +15,45 @@ import AllBooks from '../all-books/AllBooks';
 import EditBooks from '../edit-books/EditBooks';
 import Footer from '../footer-section/Footer';
 import Auth from '../auth/Auth';
+import ProtectedRouter from '../protected-router/ProtectedRouter';
+import { UserProvider } from '../../context/UserContext';
+import Layout from '../layout/layout';
 
 export default function BookVaultApp() {
-  return (
-    <Router>
-      <div className={styles.appContainer}>
-        <NavBar />
-        <div className={styles.content}>
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/" element={<AllBooks />} />
-            <Route path="/create" element={<CreateBook />} />
-            <Route path="/edit/:id" element={<EditBooks />} />
-            <Route path="/search" element={<AllBooks />} />
-            <Route path="/auth" element={<Auth />} />
-          </Routes>
-        </div>
-        <Footer />
-      </div>
-    </Router>
+
+    const router = createBrowserRouter (
+    createRoutesFromElements(
+      <Route element={<Layout />}>
+        {/* Protected route wrapper for authentication */}
+        <Route element={ <ProtectedRouter /> }>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/" element={<AllBooks />} />
+          <Route path="/create" element={<CreateBook />} />
+          <Route path="/edit/:id" element={<EditBooks />} />
+          <Route path="/search" element={<AllBooks />} />
+        </Route>
+
+        {/* Public routes */}
+        <Route path="/auth" element={ <Auth /> } /> 
+
+        {/* Fallback for undefined routes */}
+        <Route path="*" element={
+          <div>
+            <header>
+              <h1>Path is Not Found!</h1> 
+            </header>
+            <p>
+              <a href="/auth">Back to Home</a>
+            </p>
+          </div>
+        } />
+      </Route>
+    )
   )
+
+  return (
+    <UserProvider>
+      <RouterProvider router={router} />
+    </UserProvider>
+  );
 }
