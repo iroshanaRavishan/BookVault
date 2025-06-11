b69575b7de8369ea3c68942b25bebc7144f927a7
IroshanaRavishan <bandarairoshana@gmail.com>
2025-06-12 00:24:06 +0530

    added the interface class of the PdfThumbnailRepository class

---
+++ BookVault.Backend/BookVault.Domain/Interfaces/IPdfThumbnailRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookVault.Domain.Interfaces
+{
+    public interface IPdfThumbnailRepository
+    {
+        Task<byte[]> GenerateThumbnailAsync(string filename);
+    }
+}
