148884dac1ee53f0618c6a709e4cf87cd7734203
IroshanaRavishan <bandarairoshana@gmail.com>
2025-06-13 11:51:28 +0530

    added new migrations

---
+++ BookVault.Backend/BookVault.Infrastructure/Migrations/Book/20250612132344_AddedThumbnailImagePath.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookVault.Infrastructure.Migrations.Book
+{
+    /// <inheritdoc />
+    public partial class AddedThumbnailImagePath : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ReadUrl",
+                schema: "app",
+                table: "Books",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ThumbnailPath",
+                schema: "app",
+                table: "Books",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ThumbnailPath",
+                schema: "app",
+                table: "Books");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ReadUrl",
+                schema: "app",
+                table: "Books",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+        }
+    }
+}
