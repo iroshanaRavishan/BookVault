c829a4abeb5a23cfc4a9267ec56338991ae8b7fc
IroshanaRavishan <bandarairoshana@gmail.com>
2025-06-25 13:52:49 +0530

    add a separate component for password field

---
+++ BookVault.Frontend/src/components/password-input/PasswordInput.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { IoEyeOff, IoEye } from 'react-icons/io5';
+import styles from './passwordinput.module.css';
+
+export default function PasswordInput({ name, value, onChange, placeholder, required = false, className }) {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisibility = () => setVisible((prev) => !prev);
+
+  return (
+    <div className={styles.inputContainer}>
+      <div className={styles.passwordWrapper}>
+        <input
+          type={visible ? 'text' : 'password'}
+          name={name}
+          value={value}
+          placeholder={placeholder}
+          onChange={onChange}
+          className={className}
+          required={required}
+        />
+        <span className={styles.toggleIcon} onClick={toggleVisibility}>
+          {visible ? <IoEyeOff size={20} /> : <IoEye size={20} />}
+        </span>
+      </div>
+    </div>
+  );
+}
