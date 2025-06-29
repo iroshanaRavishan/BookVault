47ea3b1264065f1cc209aaa5add8424600ffa932
IroshanaRavishan <bandarairoshana@gmail.com>
2025-06-29 22:38:42 +0530

    add loading key word

--- BookVault.Frontend/src/components/loading-animation/LoadingAnimation.jsx
+++ BookVault.Frontend/src/components/loading-animation/LoadingAnimation.jsx
@@ -6,23 +6,28 @@ export function LoadingAnimation() {
 
   return (
     <div className={styles.popupOverlay}>
-        <div className={styles.bookLoader}>
-            {pages.map((lineCount, index) => (
-            <div
-                key={index}
-                className={styles.bookPage}
-                style={{
-                zIndex: pages.length - index,
-                animationDelay: `${index * 0.3}s`,
-                }}
-            >
-                <div className={styles.notes}>
-                {Array.from({ length: lineCount }).map((_, i) => (
-                    <div key={i} className={styles.noteLine} />
-                ))}
+        <div className={styles.bookLoaderContainer}>
+            <div className={styles.bookLoader}>
+                {pages.map((lineCount, index) => (
+                <div
+                    key={index}
+                    className={styles.bookPage}
+                    style={{
+                    zIndex: pages.length - index,
+                    animationDelay: `${index * 0.3}s`,
+                    }}
+                >
+                    <div className={styles.notes}>
+                    {Array.from({ length: lineCount }).map((_, i) => (
+                        <div key={i} className={styles.noteLine} />
+                    ))}
+                    </div>
                 </div>
+                ))}
             </div>
-            ))}
+            <div className={styles.loadingText}>
+                <span className={styles.loadingWrapper}>Loading<span className={styles.dots}></span></span>
+            </div>
         </div>
     </div>
   );
