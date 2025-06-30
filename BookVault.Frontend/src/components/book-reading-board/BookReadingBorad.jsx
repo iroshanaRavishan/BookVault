d62c16cddb599f2bd377a2a6c1eae12782518b5a
IroshanaRavishan <bandarairoshana@gmail.com>
2025-06-30 23:58:04 +0530

    add use params to access the id of the book

--- BookVault.Frontend/src/components/book-reading-board/BookReadingBorad.jsx
+++ BookVault.Frontend/src/components/book-reading-board/BookReadingBorad.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import styles from './bookreadingborad.module.css';
+import { useParams } from 'react-router-dom';
 
 export default function BookReadingBorad() {
+  const { id } = useParams();
+
   return (
-    <div className={styles.container}>BookReadingBorad</div>
+    <div className={styles.container}>BookReadingBorad - { id } </div>
   )
 }
