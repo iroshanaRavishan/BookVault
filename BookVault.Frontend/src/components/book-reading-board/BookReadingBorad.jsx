4371b8113b8cdade13c0e166796bafcf2fcf2403
IroshanaRavishan <bandarairoshana@gmail.com>
2025-06-30 23:38:08 +0530

    add new component to read the books

---
+++ BookVault.Frontend/src/components/book-reading-board/BookReadingBorad.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './bookreadingborad.module.css';
+
+export default function BookReadingBorad() {
+  return (
+    <div className={styles.container}>BookReadingBorad</div>
+  )
+}
