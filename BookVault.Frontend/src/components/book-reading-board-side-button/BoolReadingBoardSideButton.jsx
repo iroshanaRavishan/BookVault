8a6b9c67adc757429354e1d593fc0432b7026ac0
IroshanaRavishan <bandarairoshana@gmail.com>
2025-07-05 11:13:23 +0530

    removed rotate class

--- BookVault.Frontend/src/components/book-reading-board-side-button/BoolReadingBoardSideButton.jsx
+++ BookVault.Frontend/src/components/book-reading-board-side-button/BoolReadingBoardSideButton.jsx
@@ -5,12 +5,10 @@ import { IoChevronUp } from 'react-icons/io5';
 const BoolReadingBoardSideButton = forwardRef(({ name, top }, ref) => {
   return (
     <div className={styles.container} style={{ top: `${top}px` }} ref={ref}>
-      <div className={styles.rotated}>
-        <span className={styles.sideButton}>
-          {name}
-          <IoChevronUp className={styles.arrowIcon} />
-        </span>
-      </div>
+      <span className={styles.sideButton}>
+        {name}
+        <IoChevronUp className={styles.arrowIcon} />
+      </span>
     </div>
   );
 });
