1ac97bd56c0ea94afffd6d274bacbe4252d3c052
IroshanaRavishan <bandarairoshana@gmail.com>
2025-07-06 18:20:23 +0530

    feat: add the new botton buttom from the side button wrapper

--- BookVault.Frontend/src/components/book-reading-board-side-button/SideButtonWrapper.jsx
+++ BookVault.Frontend/src/components/book-reading-board-side-button/SideButtonWrapper.jsx
@@ -57,6 +57,9 @@ export default function SideButtonsWrapper() {
           position="left"
         />
       ))}
+
+      <BookReadingBoardSideButton name="Ask AI" position="bottom" />
+
     </>
   );
 }
