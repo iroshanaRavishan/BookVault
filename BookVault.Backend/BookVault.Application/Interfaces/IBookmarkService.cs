bb6a9703d5380e603128dda838ff2dfb557aa630
IroshanaRavishan <bandarairoshana@gmail.com>
2025-07-13 22:05:12 +0530

    added the interface class of the Bookmark service

---
+++ BookVault.Backend/BookVault.Application/Interfaces/IBookmarkService.cs
@@ -0,0 +1,16 @@
+﻿using BookVault.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookVault.Application.Interfaces
+{
+    public interface IBookmarkService
+    {
+        Task<IEnumerable<Bookmark>> GetAllAsync(Guid userId, Guid bookId);
+        Task<Bookmark> CreateAsync(Guid userId, Guid bookId, int pageNumber, string? thumbnailPath);
+        Task<bool> DeleteAsync(Guid bookmarkId);
+    }
+}
