dff39c47092cd266922a31f5ea2a77d699d66722
IroshanaRavishan <bandarairoshana@gmail.com>
2025-07-14 21:40:30 +0530

    add an action methof ot get all the Bookmarks for a particular book and the user

--- BookVault.Backend/BookVault/Controllers/BookmarkController.cs
+++ BookVault.Backend/BookVault/Controllers/BookmarkController.cs
@@ -7,5 +7,30 @@ namespace BookVault.API.Controllers
     [ApiController]
     public class BookmarkController : ControllerBase
     {
+        private readonly IBookmarkService _bookmarkService;
+
+        public BookmarkController(IBookmarkService bookmarkService)
+        {
+            _bookmarkService = bookmarkService;
+        }
+
+        // GET: api/bookmarks?userId={userId}&bookId={bookId}
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BookmarkResponseDto>>> GetAll([FromQuery] Guid userId, [FromQuery] Guid bookId)
+        {
+            var bookmarks = await _bookmarkService.GetAllAsync(userId, bookId);
+
+            var result = bookmarks.Select(b => new BookmarkResponseDto
+            {
+                Id = b.Id,
+                UserId = b.UserId,
+                BookId = b.BookId,
+                PageNumber = b.PageNumber,
+                CreatedAt = b.CreatedAt,
+                BookmarkThumbnailPath = b.BookmarkThumbnailPath
+            });
+
+            return Ok(result);
+        }
     }
 }
