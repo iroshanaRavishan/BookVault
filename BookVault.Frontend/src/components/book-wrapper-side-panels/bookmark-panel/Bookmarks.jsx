edbf33b4a9a2b466b96817971b822b19d837f419
IroshanaRavishan <bandarairoshana@gmail.com>
2025-07-15 23:50:52 +0530

    add book id on the bookamrk panel

--- BookVault.Frontend/src/components/book-wrapper-side-panels/bookmark-panel/Bookmarks.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/bookmark-panel/Bookmarks.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import styles from './bookmarks.module.css';
+import { useParams } from 'react-router-dom';
+
 
 export default function Bookmarks() {
+  const { bookid } =useParams(); 
   return (
     <div>Bookmark panel</div>
   )
