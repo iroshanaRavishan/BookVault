2bfb3f96594bc5384a2d13b8efbb22abfa600975
IroshanaRavishan <bandarairoshana@gmail.com>
2025-07-16 13:35:32 +0530

    add openedAt key to ensures to send  different values every time to "Bookmarks", to fully re-render the component and useEffect re-runs.

--- BookVault.Frontend/src/components/book-wrapper-side-panels/bookmark-panel/Bookmarks.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/bookmark-panel/Bookmarks.jsx
@@ -3,11 +3,11 @@ import styles from './bookmarks.module.css';
 import { useParams } from 'react-router-dom';
 import { useUser } from '../../../context/UserContext';
 
-export default function Bookmarks() {
-  const { bookid } =useParams(); 
+export default function Bookmarks({ openedAt }) {
+  const { id } =useParams(); 
   const {user} = useUser();
   
   return (
-    <div>Bookmark panel {user.id} - {bookid}</div>
+    <div>Bookmark panel</div>
   )
 }
