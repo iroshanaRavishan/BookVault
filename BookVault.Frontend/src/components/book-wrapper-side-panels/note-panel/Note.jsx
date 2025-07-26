91326a3574043f6fd857944f63eaeaa093b19549
IroshanaRavishan <bandarairoshana@gmail.com>
2025-07-26 13:18:02 +0530

    add the most basic work formatting

--- BookVault.Frontend/src/components/book-wrapper-side-panels/note-panel/Note.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/note-panel/Note.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill-new';
+import 'react-quill-new/dist/quill.snow.css';
 import styles from './note.module.css';
 
 export default function Note() {
+    const [content, setContent] = useState('');
+
+    const modules = {
+        toolbar: [
+            [{ 'header': [1, 2, 3, false] }],
+            ['bold', 'italic', 'underline', 'strike'],
+        ]
+    };
+
+    const formats = [
+        'header',
+        'bold', 'italic', 'underline', 'strike',
+    ];
+
   return (
-    <div>Note</div>
-  )
+    <div className={styles.noteWrapper}>
+        {/* Editor */}
+        <ReactQuill
+            value={content}
+            onChange={setContent}
+            modules={modules}
+            formats={formats}
+            placeholder="Type your note here..."
+            className={styles.editor}
+        />
+    </div>
+  );
 }
