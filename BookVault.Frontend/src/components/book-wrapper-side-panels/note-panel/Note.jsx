1643952fd70df0495aa090ca814bbb1aa9e26d6e
IroshanaRavishan <bandarairoshana@gmail.com>
2025-07-27 22:54:02 +0530

    add useLayoutEffect function

--- BookVault.Frontend/src/components/book-wrapper-side-panels/note-panel/Note.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/note-panel/Note.jsx
@@ -35,6 +35,11 @@ export default function Note({ isPanelPinned }) {
     ];
 
 
+    useLayoutEffect(() => {
+        updateTooltipPosition();
+    }, [lineHeight]);
+
+
     useEffect(() => {
         if (!quillRef.current) return;
 
