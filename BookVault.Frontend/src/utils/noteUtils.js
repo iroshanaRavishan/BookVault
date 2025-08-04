1089663269e89013f9726d98f45237080efc083f
IroshanaRavishan <bandarairoshana@gmail.com>
2025-08-04 15:49:27 +0530

    remove the note utill class

--- BookVault.Frontend/src/utils/noteUtils.js
+++
@@ -1,8 +0,0 @@
-export const confirmUnsavedChanges = (hasUnsavedChanges) => {
-  if (hasUnsavedChanges) {
-    const shouldContinue = window.confirm("You have unsaved notes. Do you want to discard them?");
-    if (!shouldContinue) return false;
-  }
-
-  return true;
-};
