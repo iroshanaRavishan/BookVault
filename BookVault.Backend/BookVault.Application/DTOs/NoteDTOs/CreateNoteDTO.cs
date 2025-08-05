5577ccc05500577dfc8b64380084d4b8c05988fd
IroshanaRavishan <bandarairoshana@gmail.com>
2025-08-05 23:16:08 +0530

    removed updateAt field from the Create DTO

--- BookVault.Backend/BookVault.Application/DTOs/NoteDTOs/CreateNoteDTO.cs
+++ BookVault.Backend/BookVault.Application/DTOs/NoteDTOs/CreateNoteDTO.cs
@@ -13,6 +13,5 @@ namespace BookVault.Application.DTOs.NoteDTOs
         public int PageNumber { get; set; }
         public required string? Content { get; set; }
         public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
-        public DateTimeOffset UpdatedAt { get; set; } = DateTimeOffset.UtcNow;
     }
 }
