3997358c7e06a9639068448801a2d98518df96f2
IroshanaRavishan <bandarairoshana@gmail.com>
2025-08-06 11:48:00 +0530

    add the implementation of the INoteRepository

---
+++ BookVault.Backend/BookVault.Infrastructure/Repositories/NoteRepository.cs
@@ -0,0 +1,29 @@
+﻿using BookVault.Application.DTOs.NoteDTOs;
+using BookVault.Application.Interfaces;
+using BookVault.Domain.Entities;
+using BookVault.Domain.Interfaces;
+using BookVault.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookVault.Infrastructure.Repositories
+{
+    public class NoteRepository : INoteRepository
+    {
+        private readonly NoteDbContext _noteDbContext;
+        public NoteRepository(NoteDbContext noteDbContext)
+        {
+            _noteDbContext = noteDbContext;
+        }
+
+        public async Task<Note> AddAsync(Note note)
+        {
+            await _noteDbContext.Notes.AddAsync(note);
+            await _noteDbContext.SaveChangesAsync();
+            return note;
+        }
+    }
+}
