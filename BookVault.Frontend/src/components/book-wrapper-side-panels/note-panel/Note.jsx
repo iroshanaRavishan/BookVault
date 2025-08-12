d1a26caf079de41602101b2753c6cda981dc36f1
IroshanaRavishan <bandarairoshana@gmail.com>
2025-08-13 02:23:20 +0530

    add additional margin top to make the alignment clear

--- BookVault.Frontend/src/components/book-wrapper-side-panels/note-panel/Note.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/note-panel/Note.jsx
@@ -658,8 +658,8 @@ export default function Note({ isPanelPinned, currentPageInfo }) {
                     {(quillRef.current?.getEditor().getLength() - 1) || 0} / {USER_NOTES.MAX_CHARS} characters
                 </span> 
                 <div >
-                    <button ><BiSolidEraser /></button>
-                    <button ><AiOutlineDelete /></button>
+                    <button ><BiSolidEraser style={{marginTop: '1px'}} /></button>
+                    <button ><AiOutlineDelete style={{marginTop: '1px'}} /></button>
                 </div>
             </div>
         </div>
