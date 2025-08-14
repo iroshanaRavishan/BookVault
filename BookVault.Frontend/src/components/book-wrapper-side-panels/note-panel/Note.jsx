36161df8c2a3e6a9aa463ff032dfa19ed0a58484
IroshanaRavishan <bandarairoshana@gmail.com>
2025-08-14 12:40:17 +0530

    set has changes true

--- BookVault.Frontend/src/components/book-wrapper-side-panels/note-panel/Note.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/note-panel/Note.jsx
@@ -508,6 +508,7 @@ export default function Note({ isPanelPinned, currentPageInfo }) {
     const handleClear = () => {
         setContent('');
         setNoteContent('');
+        setHasChanges(true);
     };
 
   return (
