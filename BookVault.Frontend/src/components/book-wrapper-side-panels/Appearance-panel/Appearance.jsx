7bef4f61b5d62f4f2a6e6af351e4334d49608d5d
IroshanaRavishan <bandarairoshana@gmail.com>
2025-08-18 09:40:29 +0530

    update CSS variable globally accoring to user input

--- BookVault.Frontend/src/components/book-wrapper-side-panels/Appearance-panel/Appearance.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/Appearance-panel/Appearance.jsx
@@ -4,8 +4,20 @@ import styles from './appearance.module.css';
 export default function Appearance() {
   const [color, setColor] = useState("#f1c40f"); // default yellow
 
+  const handleColorChange = (e) => {
+    const newColor = e.target.value;
+    setColor(newColor);
+
+    // update CSS variable globally
+    document.documentElement.style.setProperty("--flipbook-bg", newColor);
+  };
+
   return (
     <div className={styles.AppearancePanel}>
+        <div className={styles.appearanceOptions}>
+            <label>Choose FlipBook Background: </label>
+            <input type="color" value={color} onChange={handleColorChange} />
+        </div>
         <div className={styles.appearanceActions}>
             <button>Cancel</button>
             <button>Save</button>
