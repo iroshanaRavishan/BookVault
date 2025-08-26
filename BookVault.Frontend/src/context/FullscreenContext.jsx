913689c216a2dc4a7c54d6e7b24e391dff3e7a66
IroshanaRavishan <bandarairoshana@gmail.com>
2025-08-26 15:41:16 +0530

    add provider function

--- BookVault.Frontend/src/context/FullscreenContext.jsx
+++ BookVault.Frontend/src/context/FullscreenContext.jsx
@@ -2,3 +2,19 @@ import { createContext, useContext, useState, useEffect } from "react";
 
 // Create the context
 const FullscreenContext = createContext();
+
+// Provider component
+export const FullscreenProvider = ({ children }) => {
+  const [isFullScreen, setIsFullScreen] = useState(false);
+
+  const handleFullScreenToggle = () => {
+    if (!isFullScreen) {
+      document.documentElement.requestFullscreen?.();
+      setIsFullScreen(true);
+    } else {
+      document.exitFullscreen?.();
+      setIsFullScreen(false);
+    }
+  };
+
+};
