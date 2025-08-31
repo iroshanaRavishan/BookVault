fe6699d242928589a5fe3e886d446ca96d77bf4f
IroshanaRavishan <bandarairoshana@gmail.com>
2025-08-31 21:49:42 +0530

    change the maxWidth of the book widget

--- BookVault.Frontend/src/components/flip-book-method-one/FlipBook.jsx
+++ BookVault.Frontend/src/components/flip-book-method-one/FlipBook.jsx
@@ -649,7 +649,7 @@ export default function FlipBook({
         style={{ filter: `brightness(var(--flipbook-brightness))` }}
         height={345}
         minWidth={180}
-        maxWidth={isFullScreen ? 568 : 460}
+        maxWidth={isFullScreen ? 556 : 460}
         minHeight={270}
         maxHeight={isFullScreen ? 834 : 690}
         size="stretch"
