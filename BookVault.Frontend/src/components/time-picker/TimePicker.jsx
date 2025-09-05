421414947994845d63b573ba7aa7aea8b5c56580
IroshanaRavishan <bandarairoshana@gmail.com>
2025-09-05 21:39:23 +0530

    add onSet as props in TImePicker

--- BookVault.Frontend/src/components/time-picker/TimePicker.jsx
+++ BookVault.Frontend/src/components/time-picker/TimePicker.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from './timepicker.module.css';
 
-export default function TimePicker({ isAutoThemeEnabled }) {
+export default function TimePicker({ isAutoThemeEnabled, onSet }) {
   return (
     <div className={styles.container}>
       Time Picker
