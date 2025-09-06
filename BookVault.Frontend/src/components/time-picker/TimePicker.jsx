f99f3f8dc32813fd23fb7e7fdcdddfb5013680d5
IroshanaRavishan <bandarairoshana@gmail.com>
2025-09-06 15:52:05 +0530

    define hrours , mins, AM and PM.

--- BookVault.Frontend/src/components/time-picker/TimePicker.jsx
+++ BookVault.Frontend/src/components/time-picker/TimePicker.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import styles from './timepicker.module.css';
 
 export default function TimePicker({ isAutoThemeEnabled, onSet }) {
+  const [hour, setHour] = useState(12);
+  const [minute, setMinute] = useState(0);
+  const [ampm, setAmpm] = useState("AM");
   return (
     <div className={styles.container}>
       Time Picker
