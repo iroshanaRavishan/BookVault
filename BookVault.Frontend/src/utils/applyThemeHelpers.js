112bfb20bf5b95ed74b686ded7992c6337e0fb49
IroshanaRavishan <bandarairoshana@gmail.com>
2025-09-12 19:00:36 +0530

    Refactor: Apply page margin toggle change to the root element

--- BookVault.Frontend/src/utils/applyThemeHelpers.js
+++ BookVault.Frontend/src/utils/applyThemeHelpers.js
@@ -4,3 +4,9 @@
 export function applyColor(color) {
   document.documentElement.style.setProperty("--flipbook-bg", color);
 }
+
+// Page margin
+export function applyMargin(isEnabled) {
+  const marginValue = isEnabled ? "45px" : "0px";
+  document.documentElement.style.setProperty("--flipbook-margin", marginValue);
+}
