4de7d978e24960b1d05e4b4b4de87e5183d9d198
IroshanaRavishan <bandarairoshana@gmail.com>
2025-09-13 10:46:20 +0530

    feat(api): update getAppearance to fetch appearance settings by ID

--- BookVault.Frontend/src/utils/appearanceService.js
+++ BookVault.Frontend/src/utils/appearanceService.js
@@ -1,16 +1,15 @@
 const API_URL = "https://localhost:7157/api/Appearance";
 
-export const getAppearance = async () => {
-  const response = await fetch(API_URL, {
+export const getAppearance = async (id) => {
+  const response = await fetch(`${API_URL}/${id}`, {
     method: "GET",
-    headers: {
-      "Content-Type": "application/json",
+    headers: { 
+      "Content-Type": "application/json" 
     },
   });
 
-  if (!response.ok) {
+  if (!response.ok) 
     throw new Error("Failed to fetch appearance settings");
-  }
 
   return response.json();
 };
