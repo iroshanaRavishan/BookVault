0c39be8fd6c4bd7167404c99657ff7481c6d1907
IroshanaRavishan <bandarairoshana@gmail.com>
2025-09-13 11:32:53 +0530

    add fetchOrCreateAppearance effect hook

--- BookVault.Frontend/src/components/book-wrapper-side-panels/Appearance-panel/Appearance.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/Appearance-panel/Appearance.jsx
@@ -51,6 +51,13 @@ export default function Appearance() {
   }, [fromCurrent, toCurrent, isAutoThemeEnabled]);
 
   useEffect(() => {
+    const fetchOrCreateAppearance = async () => {
+    };
+
+    fetchOrCreateAppearance();
+  }, []);
+
+  useEffect(() => {
     if (!appearanceId || !fromCurrent || !toCurrent) return;
 
     const payload = {
