8e1c46aaba70c4c8606f0007c6caada130bbb329
IroshanaRavishan <bandarairoshana@gmail.com>
2025-09-14 11:51:28 +0530

    add to the ThemeHelper method to change according to the theme

--- BookVault.Frontend/src/utils/applyThemeHelpers.js
+++ BookVault.Frontend/src/utils/applyThemeHelpers.js
@@ -105,6 +105,8 @@ export function applyTheme(isDark) {
     document.documentElement.style.setProperty("--appearance-toggle-bg-unchecked-color", "#8f8f8fff");
     document.documentElement.style.setProperty("--appearance-toggle-bg-checked-color", "#ddddddff");
 
+    document.documentElement.style.setProperty("--appearance-confirm-button-border-color", "#8f8f8fff");
+    document.documentElement.style.setProperty("--appearance-confirm-button-text-disabled-color", "#ffffffff");
   } else {
     // Light theme overrides
     document.documentElement.style.setProperty("--header-pin-icon-color", "black");
@@ -169,5 +171,8 @@ export function applyTheme(isDark) {
     document.documentElement.style.setProperty("--appearance-toggle-slider-border-color", "#000");
     document.documentElement.style.setProperty("--appearance-toggle-bg-unchecked-color", "#ccc");
     document.documentElement.style.setProperty("--appearance-toggle-bg-checked-color", "#797979");
+
+    document.documentElement.style.setProperty("--appearance-confirm-button-border-color", "#bbbbbbff");
+    document.documentElement.style.setProperty("--appearance-confirm-button-text-disabled-color", "#8f8f8fff");
   }
 }
