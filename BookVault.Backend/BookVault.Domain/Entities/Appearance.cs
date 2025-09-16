2a723eeb9d80736d5c048966cad984ac80fdd4df
IroshanaRavishan <bandarairoshana@gmail.com>
2025-09-16 11:17:11 +0530

    Add Appearance entity class

---
+++ BookVault.Backend/BookVault.Domain/Entities/Appearance.cs
@@ -0,0 +1,13 @@
+﻿using BookVault.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookVault.Domain.Entities
+{
+    public sealed class Appearance : EntityBase
+    {
+    }
+}
