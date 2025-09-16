7c567ba5a782b2030a85ea6a9865b2a213c1ba6e
IroshanaRavishan <bandarairoshana@gmail.com>
2025-09-16 11:34:56 +0530

    static factory method signature

--- BookVault.Backend/BookVault.Domain/Entities/Appearance.cs
+++ BookVault.Backend/BookVault.Domain/Entities/Appearance.cs
@@ -50,5 +50,13 @@ namespace BookVault.Domain.Entities
             FromTime = fromTime;
             ToTime = toTime;
         }
+
+        // Static factory
+        public static Appearance Create(Guid userId, string color, bool marginEnabled, double brightness,
+            bool isDarkTheme, bool isDimmed, bool isFocusMode,
+            bool isAutoThemeEnabled)
+        {
+           
+        }
     }
 }
