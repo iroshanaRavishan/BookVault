7098c67371711df1b690f1da5ffd52d8e23fb65e
IroshanaRavishan <bandarairoshana@gmail.com>
2025-09-17 11:04:34 +0530

    ensure ToTime id a valid time string

--- BookVault.Backend/BookVault.Domain/Entities/Appearance.cs
+++ BookVault.Backend/BookVault.Domain/Entities/Appearance.cs
@@ -98,6 +98,9 @@ namespace BookVault.Domain.Entities
             // Optional: validate format like "09:00 PM"
             if (!DateTime.TryParse(fromTime, out _))
                 throw new ArgumentException("FromTime must be a valid time string.", nameof(fromTime));
+
+            if (!DateTime.TryParse(toTime, out _))
+                throw new ArgumentException("ToTime must be a valid time string.", nameof(toTime));
         }
     }
 }
