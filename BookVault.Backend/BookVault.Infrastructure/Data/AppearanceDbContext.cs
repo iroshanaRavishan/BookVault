98af9cbbca206d3c7f308bcdb2c47a76358399b1
IroshanaRavishan <bandarairoshana@gmail.com>
2025-09-20 11:20:23 +0530

    add AppearanceDbContext class with constructor

---
+++ BookVault.Backend/BookVault.Infrastructure/Data/AppearanceDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BookVault.Domain.Entities;
+using BookVault.Infrastructure.Data.Configurations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookVault.Infrastructure.Data
+{
+    public class AppearanceDbContext(DbContextOptions<AppearanceDbContext> options) 
+        : DbContext(options)
+    {
+    }
+}
