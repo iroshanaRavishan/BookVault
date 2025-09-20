b4bb6a7d9a617502aa64ce053771899b4c0573fe
IroshanaRavishan <bandarairoshana@gmail.com>
2025-09-20 10:35:48 +0530

    add color, margin, brightness, and theme flags

--- BookVault.Backend/BookVault.Application/DTOs/AppearanceDTOs/AppearanceReadDto.cs
+++ BookVault.Backend/BookVault.Application/DTOs/AppearanceDTOs/AppearanceReadDto.cs
@@ -10,5 +10,12 @@ namespace BookVault.Application.DTOs.AppearanceDTOs
     {
         public Guid Id { get; set; }   // From EntityBase
         public Guid UserId { get; set; }
+        public string Color { get; set; }
+        public bool MarginEnabled { get; set; }
+        public double Brightness { get; set; }
+        public bool IsDarkTheme { get; set; }
+        public bool IsDimmed { get; set; }
+        public bool IsFocusMode { get; set; }
+        public bool IsAutoThemeEnabled { get; set; }
     }
 }
