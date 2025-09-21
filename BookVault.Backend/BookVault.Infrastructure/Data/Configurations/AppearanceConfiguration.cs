35d1d9960f6d9df8c46f4506d3254c72ae417ee5
IroshanaRavishan <bandarairoshana@gmail.com>
2025-09-22 01:32:07 +0530

    add AppearanceConfiguration class implementing IEntityTypeConfiguration

---
+++ BookVault.Backend/BookVault.Infrastructure/Data/Configurations/AppearanceConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using BookVault.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookVault.Infrastructure.Data.Configurations
+{
+    public class AppearanceConfiguration : IEntityTypeConfiguration<Appearance>
+    {
+        public void Configure(EntityTypeBuilder<Appearance> builder)
+        {
+        }
+    }
+}
