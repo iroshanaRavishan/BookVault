21d14915f9ba831934235c5021da3a519c863f9d
IroshanaRavishan <bandarairoshana@gmail.com>
2025-09-23 21:33:00 +0530

    add IAppearanceRepository interface for appearance entity

---
+++ BookVault.Backend/BookVault.Domain/Interfaces/IAppearanceRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookVault.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookVault.Domain.Interfaces
+{
+    public interface IAppearanceRepository
+    {
+    }
+}
