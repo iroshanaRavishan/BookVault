7fb5e8d2c276b0e85866e61933afc1406e77cb7c
IroshanaRavishan <bandarairoshana@gmail.com>
2025-09-23 21:40:38 +0530

    add AppearanceRepository class skeleton

---
+++ BookVault.Backend/BookVault.Infrastructure/Repositories/AppearanceRepository.cs
@@ -0,0 +1,16 @@
+﻿using BookVault.Domain.Entities;
+using BookVault.Domain.Interfaces;
+using BookVault.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookVault.Infrastructure.Repositories
+{
+    public class AppearanceRepository
+    {
+    }
+}
