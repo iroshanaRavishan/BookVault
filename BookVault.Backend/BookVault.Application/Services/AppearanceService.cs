6461360002e4ae99f8ab2df01903c9f9279f4581
IroshanaRavishan <bandarairoshana@gmail.com>
2025-09-24 13:37:13 +0530

    add IAppearanceService implementation

--- BookVault.Backend/BookVault.Application/Services/AppearanceService.cs
+++ BookVault.Backend/BookVault.Application/Services/AppearanceService.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace BookVault.Application.Services
 {
-    public class AppearanceService
+    public class AppearanceService : IAppearanceService
     {
     }
 }
