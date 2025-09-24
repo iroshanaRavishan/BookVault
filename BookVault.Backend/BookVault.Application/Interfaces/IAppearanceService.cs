d7f2223877d17c883b23a09035642785a9511d21
IroshanaRavishan <bandarairoshana@gmail.com>
2025-09-24 13:08:16 +0530

    add IAppearanceService interface skeleton

---
+++ BookVault.Backend/BookVault.Application/Interfaces/IAppearanceService.cs
@@ -0,0 +1,13 @@
+﻿using BookVault.Application.DTOs.AppearanceDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookVault.Application.Interfaces
+{
+    public interface IAppearanceService
+    {
+    }
+}
