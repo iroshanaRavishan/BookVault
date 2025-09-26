f483a46e82786a8ef5b98baf72d8dbc8ebe83500
IroshanaRavishan <bandarairoshana@gmail.com>
2025-09-26 22:01:30 +0530

    add space for timeSpentDetailsChartSection

--- BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/TotalTime.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/TotalTime.jsx
@@ -12,6 +12,8 @@ export default function TotalTime() {
             <span className={styles.timeSpentValue}>5D 2h 56m</span>
             <span className={styles.timeSpentComparison}>+2h 30m than last week</span>
           </div>
+          <div className={styles.timeSpentDetailsChartSection}>
+          </div>
         </div>
       </div>
   )
