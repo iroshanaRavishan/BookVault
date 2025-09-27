4098d4adbc11d6c64eb642c0ddd4d63799f7f87b
IroshanaRavishan <bandarairoshana@gmail.com>
2025-09-27 23:03:03 +0530

    import Total TIme component

--- BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/Statistics.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/Statistics.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './statistics.module.css'
 import QuickOverview from './statistics-widgets/QuickOverview'
+import TotalTime from './statistics-widgets/TotalTime'
 
 export default function Statistics() {
   return (
