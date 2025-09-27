73b68f1d3821b4450084c97abf37cea9ddf76c32
IroshanaRavishan <bandarairoshana@gmail.com>
2025-09-27 22:54:07 +0530

    space for lastWeekSection

--- BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/TotalTimeComparison.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/TotalTimeComparison.jsx
@@ -4,6 +4,8 @@ import styles from './totaltimecomparison.module.css';
 export default function TotalTimeComparison() {
   return (
     <div className={styles.totalTimeComparisonSection}>
+        <div className={styles.lastWeekSection}>
+        </div>
     </div>
   )
 }
