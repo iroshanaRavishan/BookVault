827c034fe01c71302a5a43c23de97c7ee20f997d
IroshanaRavishan <bandarairoshana@gmail.com>
2025-10-03 10:43:56 +0530

    Configure API routing with [ApiController] attributes

--- BookVault.Backend/BookVault/Controllers/AppearanceController.cs
+++ BookVault.Backend/BookVault/Controllers/AppearanceController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace BookVault.API.Controllers
 {
     [Route("api/[controller]")]
+    [ApiController]
     public class AppearanceController : ControllerBase
     {
     }
