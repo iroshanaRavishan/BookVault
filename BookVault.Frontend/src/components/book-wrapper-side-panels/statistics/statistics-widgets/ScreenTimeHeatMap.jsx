4e41e3200f72c976aff26260bf0ed1794a146414
IroshanaRavishan <bandarairoshana@gmail.com>
2025-10-04 19:49:52 +0530

    separate place for totalScreenTimeSection

--- BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/ScreenTimeHeatMap.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/ScreenTimeHeatMap.jsx
@@ -3,6 +3,7 @@ import styles from './screentimeheatmap.module.css';
 
 export default function ScreenTimeHeatMap() {
   return (
-    <div>ScreenTimeHeatMap</div>
+    <div className={styles.totalScreenTimeSection}>  
+    </div>
   )
 }
