d358c81d304a237d91d6a522b333f02627ac8fa5
IroshanaRavishan <bandarairoshana@gmail.com>
2025-10-06 21:18:31 +0530

    data for methods signature from the Body

--- BookVault.Backend/BookVault/Controllers/AppearanceController.cs
+++ BookVault.Backend/BookVault/Controllers/AppearanceController.cs
@@ -32,7 +32,7 @@ namespace BookVault.API.Controllers
         }
 
         [HttpPost]
-        public Task<ActionResult<AppearanceReadDto>> Create()
+        public Task<ActionResult<AppearanceReadDto>> Create([FromBody] AppearanceCreateDto dto)
         {
             throw new NotImplementedException();
         }
