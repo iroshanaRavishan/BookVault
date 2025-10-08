743c40674047329684d38d9a5dfba3cecb53b449
IroshanaRavishan <bandarairoshana@gmail.com>
2025-10-08 22:49:40 +0530

    add a header section

--- BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/goal/Goal.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/goal/Goal.jsx
@@ -4,6 +4,8 @@ import styles from './goal.module.css';
 export default function Goal() {
   return (
     <div className={styles.goalSection}>  
+      <div className={styles.headerSection}>
+      </div>
     </div>
   )
 }
