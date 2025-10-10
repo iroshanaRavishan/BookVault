105750f726955bdceef700a5ee4fcbb40aa9e339
IroshanaRavishan <bandarairoshana@gmail.com>
2025-10-10 12:27:30 +0530

    add random data generator for visual testing

--- BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/screen-time-heatmap/ScreenTimeHeatMap.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/screen-time-heatmap/ScreenTimeHeatMap.jsx
@@ -4,6 +4,12 @@ import styles from './screentimeheatmap.module.css';
 export default function ScreenTimeHeatMap() {
   const days = ["2", "3", "4", "5", "6", "7", "8"];
   const times = ["0", "2", "4", "6", "8", "10", "12", "2", "4", "6", "8", "10"];
+
+  // Just random data for visual testing (0–4 intensity)
+  const generateRandomData = () =>
+    Array.from({ length: 7 }, () =>
+      Array.from({ length: 12 }, () => Math.floor(Math.random() * 10))
+    );
   return (
     <div className={styles.totalScreenTimeSection}>  
       <div className={styles.headerSection}>
