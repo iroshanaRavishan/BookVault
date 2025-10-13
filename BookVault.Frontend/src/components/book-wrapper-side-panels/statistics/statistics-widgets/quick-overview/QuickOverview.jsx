bfc2069035426806277c473da03d3ceb1dc35e97
IroshanaRavishan <bandarairoshana@gmail.com>
2025-10-13 13:50:49 +0530

    add ai indicator

--- BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/quick-overview/QuickOverview.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/quick-overview/QuickOverview.jsx
@@ -11,7 +11,10 @@ export default function QuickOverview() {
         <div className={styles.contentSectionLeftDetails}>
           <div className={styles.summaryContent}>
             <span className={styles.aiGeneratedContentText}>You are active in 56% than last week.</span>
-            <span className={styles.aiGeneratedIndicator}>Generated content</span>
+            <span className={styles.aiGeneratedIndicator}>
+              <PiRobotLight />
+              Generated content
+            </span>
           </div>
           <div className={styles.summaryContent}>
             <span>Total Pages - 43</span>
