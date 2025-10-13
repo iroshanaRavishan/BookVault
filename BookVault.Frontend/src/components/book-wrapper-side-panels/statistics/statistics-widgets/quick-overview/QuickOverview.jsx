06a0a1d85a8b6214c97e294467bdebda76adec86
IroshanaRavishan <bandarairoshana@gmail.com>
2025-10-13 13:51:02 +0530

    import react icons

--- BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/quick-overview/QuickOverview.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/quick-overview/QuickOverview.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import styles from './quickoverview.module.css'
+import styles from './quickoverview.module.css';
+import { PiRobotLight } from "react-icons/pi";
 
 export default function QuickOverview() {
   return (
