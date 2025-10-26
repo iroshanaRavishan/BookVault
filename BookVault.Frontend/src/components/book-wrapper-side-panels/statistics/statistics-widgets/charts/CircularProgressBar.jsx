4f886aae9d9a55388c041fd07b689fa2ed15ebe7
IroshanaRavishan <bandarairoshana@gmail.com>
2025-10-26 18:38:30 +0530

    import chart from chart.jd

--- BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/charts/CircularProgressBar.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/charts/CircularProgressBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Doughnut } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 
 const CircularProgressBar = () => {
   return (
