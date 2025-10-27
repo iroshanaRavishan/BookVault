205558d73e2e8dc6c670e2427fc4ee754ccb0f35
IroshanaRavishan <bandarairoshana@gmail.com>
2025-10-27 11:47:49 +0530

    add contentSectionLeftDetails wrapper

--- BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/quick-overview/QuickOverview.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/quick-overview/QuickOverview.jsx
@@ -18,6 +18,7 @@ export default function QuickOverview() {
           </span>
         </div>
         <div className={styles.contentSectionDetails}>
+          <div className={styles.contentSectionLeftDetails}>
           <div className={styles.summaryContent}>
             <span className={styles.totaPageText}>Total Pages - 43</span>
             <span className={styles.progressText}>56.4%</span>
