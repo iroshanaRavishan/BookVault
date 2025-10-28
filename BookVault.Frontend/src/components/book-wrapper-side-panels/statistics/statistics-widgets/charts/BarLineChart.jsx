45382fd145ffd2808a28abcc4b51f64925436fb1
IroshanaRavishan <bandarairoshana@gmail.com>
2025-10-28 12:07:25 +0530

    register  BarElement, LineElement and PointElement for the chart

--- BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/charts/BarLineChart.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/charts/BarLineChart.jsx
@@ -11,6 +11,12 @@ import {
 } from "chart.js";
 import { Chart } from "react-chartjs-2";
 
+ChartJS.register(
+  BarElement,
+  LineElement,
+  PointElement,
+);
+
 const BarLineChart = () => {
 
   return (
