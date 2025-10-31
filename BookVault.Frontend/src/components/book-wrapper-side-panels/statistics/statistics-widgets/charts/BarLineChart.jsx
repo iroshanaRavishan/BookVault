bccc27318cf8f3df49308221ec936fb00c5d1b22
IroshanaRavishan <bandarairoshana@gmail.com>
2025-10-31 21:25:44 +0530

    add hart options

--- BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/charts/BarLineChart.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/charts/BarLineChart.jsx
@@ -26,6 +26,11 @@ const BarLineChart = () => {
 
   const labels = ["12", "13", "14", "15", "16", "17", "18"];
 
+  const options = {
+    responsive: true,
+  };
+
+
   return (
     <div >
       <Chart type="bar" />
