ec6628345cc51b5b3d0d9791c2fe6a6c29e8aa61
IroshanaRavishan <bandarairoshana@gmail.com>
2025-11-01 21:53:42 +0530

    add border width and pont radius

--- BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/charts/BarLineChart.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/charts/BarLineChart.jsx
@@ -40,6 +40,8 @@ const BarLineChart = () => {
         type: "line",
         data: [19, 9, 5, 11, 7, 13, 10],
         borderColor: "#ff0000",
+        borderWidth: 2,
+        pointRadius: 2,
         pointBackgroundColor: "#ff0000",
       },
     ],
