ba71505c04da8e0a92b3d3ba63f3928c8ca2fe09
IroshanaRavishan <bandarairoshana@gmail.com>
2025-11-05 19:07:00 +0530

    add fonts size to the percentage mark

--- BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/charts/CircularProgressBar.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/charts/CircularProgressBar.jsx
@@ -77,7 +77,7 @@ const CircularProgressBar = ({ progress }) => {
           color: '#8a8a8aff',
         }}
       >
-        {`${progress}`} <small>%</small>
+        {`${progress}`} <small style={{fontSize: '10px'}}>%</small>
       </div>
     </div>
   );
