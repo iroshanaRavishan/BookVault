fa3470d2efbe8c2e057fcc2b72e3870a81e7f109
IroshanaRavishan <bandarairoshana@gmail.com>
2025-11-05 19:12:30 +0530

    import circularProgressBar chart

--- BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/total-time-comparison/TotalTimeComparison.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/total-time-comparison/TotalTimeComparison.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './totaltimecomparison.module.css';
+import CircularProgressBar from '../charts/CircularProgressBar';
 
 export default function TotalTimeComparison() {
   return (
