4a0950dbb69798ac01dce23c2de7c22dfadbfe9f
IroshanaRavishan <bandarairoshana@gmail.com>
2025-11-08 18:35:06 +0530

    change the chartRef name to barLineChartRef

--- BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/charts/BarLineChart.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/charts/BarLineChart.jsx
@@ -22,7 +22,7 @@ ChartJS.register(
 );
 
 const BarLineChart = () => {
-  const chartRef = useRef(null);
+  const barLineChartRef = useRef(null);
 
   const labels = ["12", "13", "14", "15", "16", "17", "18"];
 
