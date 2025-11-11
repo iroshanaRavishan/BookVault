5f32580379fda7298afdf866e40c7257caf80446
IroshanaRavishan <bandarairoshana@gmail.com>
2025-11-11 10:04:37 +0530

    add a style class

--- BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/screen-time-heatmap/ScreenTimeHeatMap.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/screen-time-heatmap/ScreenTimeHeatMap.jsx
@@ -20,7 +20,7 @@ export default function ScreenTimeHeatMap() {
         <span>Total Screen Time - (Hourly basis) <small style={{color: '#969696ff', fontWeight: 500, fontSize: '14px', fontStyle: 'italic'}}>for last 7 days</small></span>
       </div>
       <div className={styles.chartContainer}>
-        <span >
+        <span className={styles.chartIndicator} >
           Date <FaArrowUpLong />
           Hours <FaArrowRightLong style={{ marginTop: "2px" }} />
         </span>
