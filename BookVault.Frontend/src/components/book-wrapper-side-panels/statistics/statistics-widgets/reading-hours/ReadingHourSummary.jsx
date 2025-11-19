51c9d4c91ed94601b94f7dd25ff072bbaf54364d
IroshanaRavishan <bandarairoshana@gmail.com>
2025-11-19 22:55:11 +0530

    add styles for the wrapper

--- BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/reading-hours/ReadingHourSummary.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/reading-hours/ReadingHourSummary.jsx
@@ -16,7 +16,7 @@ export default function ReadingHourSummary() {
           <div>
             <BarLineChart showLine={false} width="300px" height="150px" values={values} labels={labels} barThicknessVal={6} targetLineValue={3.8}/>
           </div>
-          <div>
+          <div className={styles.summaryContent}>
           </div>
         </div>
     </div>
