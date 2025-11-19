d6fc06e9084b438980794da4fddc5fef1b9f7694
IroshanaRavishan <bandarairoshana@gmail.com>
2025-11-19 22:54:59 +0530

    add div wrapper for summary content

--- BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/reading-hours/ReadingHourSummary.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/reading-hours/ReadingHourSummary.jsx
@@ -16,6 +16,8 @@ export default function ReadingHourSummary() {
           <div>
             <BarLineChart showLine={false} width="300px" height="150px" values={values} labels={labels} barThicknessVal={6} targetLineValue={3.8}/>
           </div>
+          <div>
+          </div>
         </div>
     </div>
   )
