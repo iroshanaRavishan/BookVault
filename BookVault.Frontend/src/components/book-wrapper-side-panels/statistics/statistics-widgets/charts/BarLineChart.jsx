8507df425a22c147bbfcb9103b73e0ef8f07d3de
IroshanaRavishan <bandarairoshana@gmail.com>
2025-11-20 15:18:03 +0530

    stroke canvas path to render custom line

--- BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/charts/BarLineChart.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/charts/BarLineChart.jsx
@@ -142,6 +142,7 @@ const BarLineChart = ({
           ctx.lineWidth = 1;
           ctx.setLineDash([2]);
           ctx.beginPath();
+          ctx.stroke();
           ctx.restore();
         },
       }
