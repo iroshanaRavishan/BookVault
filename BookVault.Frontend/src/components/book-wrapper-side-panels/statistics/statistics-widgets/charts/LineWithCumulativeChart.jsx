9c61873cfef8f41afbffe47757d42a880cc11068
IroshanaRavishan <bandarairoshana@gmail.com>
2025-11-22 21:34:12 +0530

    add date and time react icons

--- BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/charts/LineWithCumulativeChart.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/charts/LineWithCumulativeChart.jsx
@@ -7,8 +7,8 @@ const LineWithCumulativeChart = () => {
   return (
     <div className={styles.chartContainer} style={{ width: '200px', height: '100px' }}>
       <span className={styles.chartIndicator} style={{ transform: 'translateX(50%)' }}>
-        Hours
-        Date
+        Hours <FaArrowUpLong />
+        Date  <FaArrowRightLong /> 
       </span>
     </div>
   );
