0df597d17a97aaed83e5fed0b57ddb37b4938696
IroshanaRavishan <bandarairoshana@gmail.com>
2025-11-22 21:32:39 +0530

    add date and time

--- BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/charts/LineWithCumulativeChart.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/charts/LineWithCumulativeChart.jsx
@@ -5,7 +5,10 @@ const LineWithCumulativeChart = () => {
 
   return (
     <div className={styles.chartContainer} style={{ width: '200px', height: '100px' }}>
-      <span className={styles.chartIndicator} style={{ transform: 'translateX(50%)' }}></span>
+      <span className={styles.chartIndicator} style={{ transform: 'translateX(50%)' }}>
+        Hours
+        Date
+      </span>
     </div>
   );
 };
