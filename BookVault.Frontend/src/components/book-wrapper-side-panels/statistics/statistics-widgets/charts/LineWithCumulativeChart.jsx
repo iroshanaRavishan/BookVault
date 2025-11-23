072e24457fea8d6d901258f0643810df813194b9
IroshanaRavishan <bandarairoshana@gmail.com>
2025-11-23 23:26:34 +0530

    import useRef

--- BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/charts/LineWithCumulativeChart.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/charts/LineWithCumulativeChart.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from './charts.module.css';
 import { Chart as ChartJS, LineElement, CategoryScale } from "chart.js";
 import { Chart } from "react-chartjs-2";
