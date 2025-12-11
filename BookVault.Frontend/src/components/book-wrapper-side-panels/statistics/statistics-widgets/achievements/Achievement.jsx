9a8fdbf144d1c48b422270f5b5158f7f351f24b2
IroshanaRavishan <bandarairoshana@gmail.com>
2025-12-11 23:11:41 +0530

    add troophy ocon

--- BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/achievements/Achievement.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/achievements/Achievement.jsx
@@ -141,7 +141,7 @@ export default function Achievement() {
                             <span>{item.mins} mins</span>
                           </div>
                           <div className={styles.historyItemTroophy}>
-                            <span>Troophy</span>
+                            <span><FaTrophy/></span>
                           </div>
                         </div>
                         <div className={styles.historyItemDate}>
