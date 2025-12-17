236dba8d53c613e6be7d96a6252aa7f88e8fcaec
IroshanaRavishan <bandarairoshana@gmail.com>
2025-12-17 22:36:22 +0530

    add missing brakets

--- BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/goal/Goal.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/goal/Goal.jsx
@@ -28,7 +28,7 @@ export default function Goal() {
       </div>
       <div className={styles.goalSummarySection}>
         <div className={styles.miniCharts}>
-          {["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"].map((day, index) => (
+          {["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"].map((day, index) => {
             const randomProgress = Math.floor(Math.random() * (90 - 10 + 1)) + 10;
             <div key={index} className={styles.miniChartBox}>
               <div>
