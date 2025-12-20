da957e2a25e396aee3dddf404bee2be6e80a045b
IroshanaRavishan <bandarairoshana@gmail.com>
2025-12-20 22:49:42 +0530

    add options object for cumulative chart configuration

--- BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/charts/LineWithCumulativeChart.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/charts/LineWithCumulativeChart.jsx
@@ -60,7 +60,10 @@ const LineWithCumulativeChart = () => {
       },
     ],
   };
-  
+
+  const options = {
+  };
+
   return (
     <div className={styles.chartContainer} style={{ width: '200px', height: '100px' }}>
       <span className={styles.chartIndicator} style={{ transform: 'translateX(50%)' }}>
