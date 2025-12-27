33e309180e6a58d3785d58af3f8f23679dd9cf2d
IroshanaRavishan <bandarairoshana@gmail.com>
2025-12-27 20:46:04 +0530

    Add end color stop to gradient

--- BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/charts/MiniProgressChart.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/charts/MiniProgressChart.jsx
@@ -13,6 +13,7 @@ const MiniProgressChart = ({ progress }) => {
     // Create gradient for the progress arc
     const gradient = ctx.createLinearGradient(0, 0, 0, ctx.canvas.height);
     gradient.addColorStop(0, '#97b4f5ff');
+    gradient.addColorStop(1, '#4947e2ff');
   }, [progress]);
 
   return (
