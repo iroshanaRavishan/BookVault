467022ce1bee33ac8c7b1c5871c5481c9bf90f1e
IroshanaRavishan <bandarairoshana@gmail.com>
2026-01-07 22:14:59 +0530

    add section name

--- BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/achievements/Achievement.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/statistics/statistics-widgets/achievements/Achievement.jsx
@@ -185,6 +185,7 @@ export default function Achievement() {
               }`}
             >
               <div>
+                <span>Ice Breakers</span>
                 <FaChevronRight size={13} />
               </div>
             </div>
