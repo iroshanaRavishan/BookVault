577b338c369cea62ece0335d0f6349be461decfb
IroshanaRavishan <bandarairoshana@gmail.com>
2026-01-18 20:12:23 +0530

    another use ref for chat Name

--- BookVault.Frontend/src/components/book-wrapper-side-panels/AskAI/AskAI.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/AskAI/AskAI.jsx
@@ -2,6 +2,7 @@ import React, { useRef } from 'react';
 
 export default function AskAI() {
   const conversationIdRef = useRef(crypto.randomUUID());
+  const hasNamedChatRef = useRef(false);
   return (
     <div>AskAI</div>
   )
