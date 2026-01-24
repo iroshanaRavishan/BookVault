c260d8153240be40175f95ccf9683c397e948aea
IroshanaRavishan <bandarairoshana@gmail.com>
2026-01-24 22:37:06 +0530

    add a use state to track the editingMsg

--- BookVault.Frontend/src/components/book-wrapper-side-panels/AskAI/ask-aI-widgets/MessageWall.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/AskAI/ask-aI-widgets/MessageWall.jsx
@@ -6,6 +6,7 @@ export default function MessageWall({ messages, isTyping }) {
   const wallRef = useRef(null);
 
   const [hoveredId, setHoveredId] = useState(null);
+  const [editingMsg, setEditingMsg] = useState(null);
   const [showScrollDown, setShowScrollDown] = useState(false);
   const [userScrolledUp, setUserScrolledUp] = useState(false);
 
