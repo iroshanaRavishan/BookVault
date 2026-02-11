135307b98cbbfabfbaf34caee99eed03d0e963b1
IroshanaRavishan <bandarairoshana@gmail.com>
2026-02-11 21:49:46 +0530

    base on ShowCancel value, display the cancel popup

--- BookVault.Frontend/src/components/book-wrapper-side-panels/AskAI/ask-aI-widgets/ChatInput.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/AskAI/ask-aI-widgets/ChatInput.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import styles from './chatinput.module.css';
 
-export default function ChatInput() {
+export default function ChatInput({ showCancel = false}) {
   return (
     <div 
       className={styles.actionWrapper}
       style={{paddingRight: isEditing ? '0px' : '4px'}}
     >
+      {showCancel && (
+        <div > 
+        </div>
+        )}
       <div 
         className={styles.textareaWrapper} 
         style={{ height: isEditing ? '200px' : '' }}
