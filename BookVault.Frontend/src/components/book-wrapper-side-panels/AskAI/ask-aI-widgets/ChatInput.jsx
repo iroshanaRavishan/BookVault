078a279d8b1ecb8da04d2aaf5761d7898bed3d60
IroshanaRavishan <bandarairoshana@gmail.com>
2026-02-11 21:58:15 +0530

    add react icon for close the edit popup

--- BookVault.Frontend/src/components/book-wrapper-side-panels/AskAI/ask-aI-widgets/ChatInput.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/AskAI/ask-aI-widgets/ChatInput.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { IoCloseCircleSharp } from 'react-icons/io5';
 import styles from './chatinput.module.css';
 import { MdModeEditOutline } from 'react-icons/md';
 
@@ -11,6 +12,7 @@ export default function ChatInput({ showCancel = false}) {
       {showCancel && (
         <div className={styles.editingHeader}> 
           <span className={styles.editingHeaderSpan}> <MdModeEditOutline size={20}/> Edit the prompt here</span>
+          <IoCloseCircleSharp />
         </div>
         )}
       <div 
