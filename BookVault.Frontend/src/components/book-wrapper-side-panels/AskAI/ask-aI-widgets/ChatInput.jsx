6dd0a71372a30bb5aad31955129b89c9605f1980
IroshanaRavishan <bandarairoshana@gmail.com>
2026-02-26 10:29:45 +0530

    import required react Icons

--- BookVault.Frontend/src/components/book-wrapper-side-panels/AskAI/ask-aI-widgets/ChatInput.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/AskAI/ask-aI-widgets/ChatInput.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { IoSend, IoChevronUp, IoCloseCircleSharp } from 'react-icons/io5';
 import styles from './chatinput.module.css';
+import { FiPaperclip } from 'react-icons/fi';
 import { MdModeEditOutline } from 'react-icons/md';
 
 export default function ChatInput({
