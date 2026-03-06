7850d06161bb04961ab6a767c3c8522bd8464904
IroshanaRavishan <bandarairoshana@gmail.com>
2026-03-06 21:41:06 +0530

    add historyActionPopupBody wrapper

--- BookVault.Frontend/src/components/book-wrapper-side-panels/AskAI/ask-aI-widgets/history-action-popup/HistoryActionPopup.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/AskAI/ask-aI-widgets/history-action-popup/HistoryActionPopup.jsx
@@ -4,6 +4,7 @@ import styles from './historyactionpopup.module.css';
 export default function HistoryActionPopup() {
   return (
     <div className={styles.historyActionPopupPanel}>
+        <div className={styles.historyActionPopupBody}>
 
     </div>
   )
