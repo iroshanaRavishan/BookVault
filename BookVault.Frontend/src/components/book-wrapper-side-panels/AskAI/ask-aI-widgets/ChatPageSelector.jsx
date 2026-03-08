a5ff46be553fadb42359bfe850dc17889e5586ad
IroshanaRavishan <bandarairoshana@gmail.com>
2026-03-08 19:24:21 +0530

    refactor: update ChatPageSelector to use currentPageInfo from FlipBookContext

--- BookVault.Frontend/src/components/book-wrapper-side-panels/AskAI/ask-aI-widgets/ChatPageSelector.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/AskAI/ask-aI-widgets/ChatPageSelector.jsx
@@ -3,6 +3,8 @@ import styles from "./chatpageselector.module.css";
 import { useFlipBook } from "../../../../context/FlipBookContext";
 
 export default function ChatPageSelector() {
+  const { currentPageInfo } = useFlipBook();
+
   return (
     <div>ChatPageSelector</div>
   );
