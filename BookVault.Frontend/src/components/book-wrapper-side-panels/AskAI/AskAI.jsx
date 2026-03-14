616de87173d4fb950a3068a83643051c0f47a9f7
IroshanaRavishan <bandarairoshana@gmail.com>
2026-03-14 22:49:50 +0530

    add useState to set the message

--- BookVault.Frontend/src/components/book-wrapper-side-panels/AskAI/AskAI.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/AskAI/AskAI.jsx
@@ -4,6 +4,8 @@ import MessageWall from './ask-aI-widgets/MessageWall';
 export default function AskAI() {
   const conversationIdRef = useRef(crypto.randomUUID());
   const hasNamedChatRef = useRef(false);
+
+  const [messages, setMessages] = useState([]);
   return (
     <div className={styles.panel}>
       <div className={styles.chatActionsIconBar}>
