eeaa55a42eb4d8e2c486ec8c939d85dc45e4104c
IroshanaRavishan <bandarairoshana@gmail.com>
2026-03-31 08:20:12 +0530

    set showing the initial UI false when loading the chat to the wall

--- BookVault.Frontend/src/components/book-wrapper-side-panels/AskAI/AskAI.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/AskAI/AskAI.jsx
@@ -83,6 +83,7 @@ export default function AskAI() {
     setCurrentChatName(chat.chatName);
     // set message goes here
     setShowHistory(false);
+    setShowInitialUI(false);
   };
 
   return (
