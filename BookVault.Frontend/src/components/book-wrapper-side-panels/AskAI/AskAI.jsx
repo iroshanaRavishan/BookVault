dd271c876eab0b9240a08b9f292e5ce9263f159b
IroshanaRavishan <bandarairoshana@gmail.com>
2026-04-01 22:14:29 +0530

    add canContinueChat state to manage chat continuation

--- BookVault.Frontend/src/components/book-wrapper-side-panels/AskAI/AskAI.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/AskAI/AskAI.jsx
@@ -17,6 +17,7 @@ export default function AskAI() {
   const [isTyping, setIsTyping] = useState(false);
   const [showInitialUI, setShowInitialUI] = useState(true);
   const [isResetting, setIsResetting] = useState(false);
+  const [canContinueChat, setCanContinueChat] = useState(false);
   const [currentChatName, setCurrentChatName] = useState("New Chat");
   const [chatList, setChatList] = useState([]);
   const [showConfirm, setShowConfirm] = useState(false);
