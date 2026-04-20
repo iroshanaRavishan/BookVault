b9d09ad60d53e80de136afc3ff3fb0e59606c1cc
IroshanaRavishan <bandarairoshana@gmail.com>
2026-04-20 23:33:16 +0530

    reset active chat ID after closing the popup

--- BookVault.Frontend/src/components/book-wrapper-side-panels/AskAI/AskAI.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/AskAI/AskAI.jsx
@@ -180,6 +180,10 @@ export default function AskAI() {
 
   const closePopup = () => {
     setShowOverlay(false); // start fade-out
+
+    setTimeout(() => {
+      setActiveChatId(null);
+    }, 200); 
   };
 
   const handleBeforeLeave = (action) => {
