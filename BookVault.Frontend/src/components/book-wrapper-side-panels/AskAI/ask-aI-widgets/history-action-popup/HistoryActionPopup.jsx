995701684b61f6e7ca1577dbb5517c3265bd5438
IroshanaRavishan <bandarairoshana@gmail.com>
2026-05-07 05:58:37 +0530

    add onDelete prop to HistoryActionPopup component

--- BookVault.Frontend/src/components/book-wrapper-side-panels/AskAI/ask-aI-widgets/history-action-popup/HistoryActionPopup.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/AskAI/ask-aI-widgets/history-action-popup/HistoryActionPopup.jsx
@@ -4,7 +4,7 @@ import { BsFillPinAngleFill, BsPinFill } from "react-icons/bs";
 import { TiExport } from "react-icons/ti";
 import { MdDelete, MdModeEdit } from 'react-icons/md';
 
-export default function HistoryActionPopup({ isPinned, onTogglePin }) {
+export default function HistoryActionPopup({ isPinned, onTogglePin, onDelete }) {
   return (
     <div
       className={styles.historyActionPopupPanel}
