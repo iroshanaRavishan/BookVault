6ce487e763ca3d2a4472fc0860c2e6c665273807
IroshanaRavishan <bandarairoshana@gmail.com>
2026-05-20 23:13:09 +0530

    pass isClosingExport prop to ExportPopup comp

--- BookVault.Frontend/src/components/book-wrapper-side-panels/AskAI/AskAI.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/AskAI/AskAI.jsx
@@ -572,6 +572,7 @@ export default function AskAI() {
         <ExportPopup
           onSelect={handleExportSelect}
           onClose={closeExportPopup}
+          isClosing={isClosingExport}
         />
       )}
     </div>
