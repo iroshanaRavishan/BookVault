0a1ee96aa86aea685ffc2a7fd50fa1f08d6bac92
IroshanaRavishan <bandarairoshana@gmail.com>
2026-06-05 22:24:34 +0530

    add useEffect to monitor editingChatId changes

--- BookVault.Frontend/src/components/book-wrapper-side-panels/AskAI/AskAI.jsx
+++ BookVault.Frontend/src/components/book-wrapper-side-panels/AskAI/AskAI.jsx
@@ -56,6 +56,10 @@ export default function AskAI() {
     }
   }, [messages]);
 
+  useEffect(() => {
+
+  }, [editingChatId]);
+
   const startNewChat = () => {
     setIsResetting(true);
     setMessages([]);
